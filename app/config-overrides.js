7f3f9bafb544902fea9733e94c676e1fe5481315
satyaprakashmohanty13 <mohanty.subash1984@gmail.com>
2025-07-12 12:26:26 +0530

    Delete app/config-overrides.js

--- app/config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = function override(config, env) {
-  // fix for tensortflow - https://github.com/tensorflow/tfjs/issues/3384
-  if (process.env.NODE_ENV === "production") {
-    config.resolve.mainFields = ["main"];
-  }
-  return config;
-};
